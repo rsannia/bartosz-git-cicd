5abf2d8c8ff6664a17d1d64238c7d843371b503e
rsannia <sannia.ulhaq@hotmail.com>
2024-01-22 14:42:33 +0100

    Failed tests CI/CD

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -40,6 +40,6 @@ def test_overdraw_checking():
 def test_overdraw_savings():
     account = Account('savings')
     account.deposit(50)
-    account.withdraw(25) # 100-> 25
-    assert account.balance == 25 # -50 -> 25
+    account.withdraw(100) # 100-> 25 
+    assert account.balance == -50 # -50 -> 25
 # weg met deze tests !!
